6f182527e29279d7ae8f24cc890ac1a588c9f627
jhigger <njohnjhigger@gmail.com>
2023-05-28 06:00:00 +0800

    font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import { Space_Mono } from "next/font/google";
+
+const font = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={font.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
